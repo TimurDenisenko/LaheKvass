c0f87e9e758158f15b77a4de147334aeb3cf9a2f
opilane <opilane@ORDI-402561>
2024-11-25 15:58:56 +0200

    DbSetExtension WIP

---
+++ Models/DbSetExtension.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
+using System.Threading.Tasks;
+namespace LaheKvass.Models
+{
+    public static class DbSetExtension
+    {
+        public static async Task<T> ElementOrDefault<T>(this DbSet<T> DB, int id) where T : DBModel =>
+            await DB.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id) ?? null;
+    }
+}
